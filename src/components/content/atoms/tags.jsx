f440b36ff974d5d9fb0fbf91e424eddd4a771cf5
Akinori Noguchi <ts-akinori.noguchi@rakuten.com>
2018-04-13 14:54:38 +0900

    Updated tag with icon

--- src/components/content/atoms/tags.jsx
+++ src/components/content/atoms/tags.jsx
@@ -2,16 +2,16 @@ import * as React from 'react';
 
 export function getTag(type, text, isDisplayedIcon) {
   return isDisplayedIcon ? (
-    <div className={`tag${type}`}>
-      <span className="content">{text}</span>
-      {isDisplayedIcon ? <span className="close" /> : null}
+    <div className={`tag-wrapper ${type}`}>
+      <span className="tag">{text}</span>
+      <span className="close" />
     </div>
-  ) : (<div className={`tag${type}`}>{text}</div>);
+  ) : (<div className={`tag ${type}`}>{text}</div>);
 }
 
 export default function Tags(props) {
   const text = props.text ? props.text : '';
-  const type = props.type ? ` ${props.type}` : '';
+  const type = props.type ? props.type : '';
   const isDisplayedIcon = type.split(' ').includes('icon');
 
   return getTag(type, text, isDisplayedIcon);
