3359dfb9934bd9b4668d6806fa0d4753f461beff
Saito, Shugo | Shugo | COED <shugo.saito@rakuten.com>
2018-04-25 20:37:31 +0900

    v0.2.2

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -21,7 +21,7 @@ export function Sidebar(props) {
     <section id="sidebar" onClick={clearSearchAction}>
       <nav style={style}>
         <Logo colorSelect={colorSelect} />
-        <div className="version">v0.1.0</div>
+        <div className="version">v0.2.2</div>
         <Menu menu={menu} localization={localization} />
       </nav>
     </section>
