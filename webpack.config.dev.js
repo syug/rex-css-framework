c1a2d1c8d099a33b2d1e85fee106cf811cc4ab92
Saito, Shugo | Shugo | COED <shugo.saito@rakuten.com>
2018-05-17 00:52:45 +0900

    Updated - added HtmlWebpackPlugin into dev settings

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,12 +1,33 @@
 const path = require('path');
-const webpackConfig = require('./webpack.config.common.js');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.config.common');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-webpackConfig.output.path = path.resolve(__dirname, 'dev');
-webpackConfig.devServer = {
+const plugins = [
+  new webpack.HotModuleReplacementPlugin(),
+  new HtmlWebpackPlugin({
+    title: 'ReX CSS Framework',
+    template: './dev/index.html',
+    excludeChunks: ['js'],
+  }),
+];
+
+const rules = [
+
+];
+
+common.output.path = path.resolve(__dirname, 'dev');
+common.devServer = {
   quiet: false,
   hot: true,
   open: true,
   openPage: 'rex-default.css',
 };
 
-module.exports = webpackConfig;
+module.exports = merge(common, {
+  plugins,
+  module: {
+    rules,
+  },
+});
