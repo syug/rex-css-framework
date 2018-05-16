4ec1ae45ee517fdec7c0116c9d530a47aba1dc8c
Saito, Shugo | Shugo | COED <shugo.saito@rakuten.com>
2018-05-16 15:48:31 +0900

    Removed - removed js files for SPA

--- src/domain/middleware/prismCustomSetting.js
+++
@@ -1,9 +0,0 @@
-import Prism from 'prismjs';
-
-require('prismjs/plugins/custom-class/prism-custom-class.js');
-
-export default function customClassName() {
-  Prism.plugins.customClass.map({
-    tag: 'prism-tag',
-  });
-}
--- src/domain/store/reduce/routerReducer.js
+++
@@ -1,5 +0,0 @@
-import { setIn } from 'domain/store/reduce/main';
-
-export function setCurrentRoute(state, RouteCurrent) {
-	return setIn(state,['router', 'current'], RouteCurrent);
-}
